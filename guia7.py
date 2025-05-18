bbc205e58d30e34b4a1bbac52bf3619cc95f7062
lulojrz <lulojrz18@gmail.com>
2025-05-18 17:47:53 -0300

    2 dia 1.0

--- guia7.py
+++ guia7.py
@@ -48,3 +48,120 @@ def suma_total(x:List[int])-> int:
         
     return contador
 
+#ejercicio 1d
+def maximo(x:List[int])-> int:
+    if not x:
+        return None
+    
+    max:int= x[0]
+    for num in range(1,len(x)):
+        if x[num] > max:
+            max = x[num]
+            
+    return max
+
+
+#ejercicio 1e
+def minimo(x:List[int])-> int:
+    if not x:
+        return None
+    
+    min:int= x[0]
+    for num in range(1,len(x)):
+        if x[num] < min:
+            min = x[num]
+            
+    return min
+
+#ejercicio 1f
+def ordenados(x:List[int])-> bool:
+    afirmacion: bool = False
+    if not x :
+        afirmacion = False
+        
+    else:
+     primero:int = x[0]
+
+     for num in range(1,len(x)):
+      if primero  > x[num]: 
+        afirmacion = False
+        break
+     else:
+       primero = x[num]  
+       afirmacion=True
+    
+    return afirmacion
+ 
+#ejercicio 1g
+def pos_maximo(x:List[int])-> int:
+    if (x):
+     maximo_num =  maximo(x)
+     pos_max = x.index(maximo_num)
+     return pos_max
+    else:
+        return -1
+
+
+#ejercicio 1h
+def pos_minimo(x:List[int])-> int:
+    if (x):
+     minimo_num =  minimo(x)
+     pos_max = x.index(minimo_num)
+     return pos_max
+    else:
+        return -1
+    
+#ejercicio 1i
+def long_mayoraSiete(x:List[str])-> int:
+    afirmacion =  False
+    if (x):
+        for elemento in x :
+            if len(elemento)>7:
+                afirmacion = True
+                break
+    else : 
+      afirmacion = False
+        
+   
+    return afirmacion 
+
+#ejericio 1k
+def esPalindroma(x:str)-> bool:
+    afirmacion = True
+    if x:
+        reverso = ""
+        for num in range(len(x)-1,-1,-1):
+            reverso+= x[num]
+            
+        if x == reverso:
+           afirmacion = True
+        else:
+            afirmacion = False
+    else : 
+        afirmacion = True
+        
+    return afirmacion
+
+
+#ejercicio 1l
+def  iguales_consecutivos(x:list[int])-> bool:
+    afirmacion = False
+    contador = 1
+    if x  and (len(x)>= 3):
+       for i in range (1,len(x)):
+           if x[i] == x[i-1]:
+               contador+=1
+     
+       if contador ==3 :
+             return True    
+       else:
+           return False
+           
+    else :
+        return False
+        
+  
+    return afirmacion
+        
+        
+        
--- tests_guia7.py
+++ tests_guia7.py
@@ -1,60 +1,47 @@
 import unittest
-from guia7 import pertenece_1,pertenece_2,pertenece_3,divide_a_todos,suma_total
-
+from guia7 import pertenece_1,pertenece_2,pertenece_3,divide_a_todos,suma_total, maximo,minimo,ordenados, pos_maximo,pos_minimo, long_mayoraSiete,esPalindroma,iguales_consecutivos
 class test_pertenece1(unittest.TestCase):
     def test_pertenece1(self):
-        resultado_esperado = True
-        self.assertTrue(pertenece_1([1,2,3],1),resultado_esperado)
+        self.assertTrue(pertenece_1([1,2,3],1))
         
     def test_nopertenece1(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_1([1,2,3],4),resultado_esperado)
+        self.assertFalse(pertenece_1([1,2,3],4))
     
     def test_listavacia1(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_1([],1),resultado_esperado)
+        self.assertFalse(pertenece_1([],1))
 
 class test_pertenece2(unittest.TestCase):
     def test_pertenece2(self):
-        resultado_esperado = True
-        self.assertTrue(pertenece_2([1,2,3],1),resultado_esperado)
+        self.assertTrue(pertenece_2([1,2,3],1))
         
     def test_nopertenece2(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_2([1,2,3],4),resultado_esperado)
+        self.assertFalse(pertenece_2([1,2,3],4))
     
     def test_listavacia2(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_2([],1),resultado_esperado)
+        self.assertFalse(pertenece_2([],1))
         
 class test_pertenece3(unittest.TestCase):
    def test_pertenece3(self):
-        resultado_esperado = True
-        self.assertTrue(pertenece_3([-1,2,3],-1),resultado_esperado)
+        self.assertTrue(pertenece_3([-1,2,3],-1))
         
    def test_nopertenece3(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_3([1,2,3],4),resultado_esperado)
+        self.assertFalse(pertenece_3([1,2,3],4))
     
    def test_listavacia3(self):
-        resultado_esperado = False
-        self.assertFalse(pertenece_3([],1),resultado_esperado)
+        self.assertFalse(pertenece_3([],1))
         
-class divide(unittest.TestCase):
+class test_divide(unittest.TestCase):
     def test_divideatodos(self):
-        resultado_esperado= True
-        self.assertTrue(divide_a_todos([2,4,6],2),resultado_esperado)
+        self.assertTrue(divide_a_todos([2,4,6],2))
         
     def test_nodivideatodos(self):
-        resultado_esperado = False
-        self.assertFalse(divide_a_todos([2,4,6],3),resultado_esperado)
+        self.assertFalse(divide_a_todos([2,4,6],3))
     
     def test_listavaciaendivide(self):
-        resultado_esperado= False
-        self.assertFalse(divide_a_todos([],2),resultado_esperado)
+        self.assertFalse(divide_a_todos([],2))
 
 
-class suma(unittest.TestCase):
+class tests_suma(unittest.TestCase):
     def test_suma_esperada(self):
         resultado_esperado = 2
         self.assertEqual(suma_total([-2,2,2]),resultado_esperado)
@@ -62,6 +49,105 @@ class suma(unittest.TestCase):
     def test_sumaincorrecta(self):
         resultado_esperado = 6
         self.assertNotEqual(suma_total([1,1,1]),resultado_esperado)
+        
+class tests_maximo(unittest.TestCase):
+    def test_maximo_esperado(self):
+        resultado_esperado = 89
+        self.assertEqual(maximo([5,2,89]),resultado_esperado)
+        
+    def test_maximo_vacio(self):
+        resultado_esperado = None
+        self.assertEqual(maximo([]),resultado_esperado)
+        
+    
+class tests_minimo(unittest.TestCase):
+    def test_minimo_esperado(self):
+        resultado_esperado = 2
+        self.assertEqual(minimo([3,43,2]),resultado_esperado)
+        
+    def test_minimo_vacio(self):
+        resultado_esperado = None
+        self.assertEqual(minimo([]),resultado_esperado)
+        
+class tests_ordenados(unittest.TestCase):
+    def test_ordenados_true(self):
+        self.assertTrue(ordenados([1,2,3]))
+        
+    def test_ordenados_false(self):
+        self.assertFalse(ordenados([3,2,45]))
+        
+    def test_ordenados_iguales(self):
+        self.assertTrue(ordenados([1,1,1]))
+        
+    def test_ordenados_vacio(self):
+        self.assertFalse(ordenados([])) 
+        
+class tests_maximo_posicion(unittest.TestCase):
+    def test_maximo_posicion_esperado_correcto(self):
+        resultado_esperado = 2
+        self.assertEqual(pos_maximo([1,2,3]), resultado_esperado)
+        
+    def test_maximo_posicion_vacio(self):
+        resultado_esperado = -1
+        self.assertEqual(pos_maximo([]),resultado_esperado)
+        
+    def test_maximo_posicion_iguales(self):
+        resultado_esperado = 0
+        self.assertEqual(pos_maximo([1,1,1]),resultado_esperado)
+        
+class tests_minimo_posicion(unittest.TestCase):
+    def test_minimo_posicion_esperado_correcto(self):
+      resultado_esperado = 2
+      self.assertEqual(pos_minimo([3,2,1]), resultado_esperado)
+      
+    def test_minimo_posicion_vacio(self):
+        resultado_esperado = -1
+        self.assertEqual(pos_minimo([]),resultado_esperado)
+        
+    def test_minimo_posicion_iguales(self):
+        resultado_esperado = 0
+        self.assertEqual(pos_minimo([1,1,1]),resultado_esperado)
+        
+class tests_longitud(unittest.TestCase):
+    def test_longitud_mayorSiete(self):
+        self.assertTrue(long_mayoraSiete(["pepe","sebastian"]))
+        
+    def test_longitud_menorSiete(self):
+        self.assertFalse(long_mayoraSiete(["pepe","gato"]))
+        
+    def test_longitud_vacio(self):
+        self.assertFalse(long_mayoraSiete([])) 
+        
+class tests_palindroma(unittest.TestCase):
+    def test_palindromo_correcto(self):
+        self.assertTrue(esPalindroma("otto"))
+    
+    def test_palindromo_incorrecto(self):
+        self.assertFalse(esPalindroma("luca"))
+        
+    def test_palindromo_caracter(self):
+        self.assertTrue(esPalindroma("a"))
+     
+    def test_palindromo_vacio(self):
+        self.assertTrue(esPalindroma(""))
+        
+class tests_iguales_consecutivos(unittest.TestCase):
+    def test_tres_consecutivos_correcto(self):
+        self.assertTrue(iguales_consecutivos([2,3,1,1,1]))
+        
+    def test_ningun_consecutivo(self):
+        self.assertFalse(iguales_consecutivos([2,4,5,6,7]))
+        
+    def test_consecutivos_lista_vacia(self):
+        self.assertFalse(iguales_consecutivos([]))
+    
+    def test_todos_consecutivos_iguales(self):
+        self.assertTrue(iguales_consecutivos([1,1,1]))
+        
+    def test_mas_detres_consecutivosiguales(self):
+        self.assertFalse(iguales_consecutivos([1,1,1,1,1,1]))
+        
+            
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
