ae6b98f8f410dc023b199a87e1fbdbeac5adf63f
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-05-20 20:57:08 -0300

    Update python2.py

--- python2.py
+++ python2.py
@@ -1,3 +1,31 @@
+def pertenece_1 (x:list[int], y:int) -> bool:
+    
+    afirmacion :bool = False 
+    for elemento in x:
+        if elemento == y:
+            afirmacion = True
+            
+    return afirmacion
+
+def ordenados(x:list[int])-> bool:
+    afirmacion: bool = False
+    if not x :
+        afirmacion = False
+        
+    else:
+     primero:int = x[0]
+
+     for num in range(1,len(x)):
+      if primero  > x[num]: 
+        afirmacion = False
+        break
+     else:
+       primero = x[num]  
+       afirmacion=True
+    
+    return afirmacion
+
+
 
 #ejercicio 3
 
@@ -52,4 +80,42 @@ def saldoActual(movimientos:list[(str,int)])->int:
     return saldo
         
 #MATRICES
-#ejercicio 5
+#ejercicio 5.1
+
+def pertenece_a_cadauno_1(x:list[list[int]],y:int)->list[bool]:
+    res: list[bool] = []
+    for lista in x :
+        if pertenece_1(lista,y) == True:
+            res.append(True)
+        else:
+            res.append(False)
+        
+    return res
+          
+
+    
+
+#ejercicio 6.1
+def esMatriz(x:list[list[int]]) ->bool:
+    res: bool = False
+    for lista in x:
+        if lista[0]:
+          res= True
+          
+    return res
+
+#ejercicio 6.2
+def filas_ordenadas(x:list[list[int]]) ->[bool]:
+    res:[bool] = []
+    if esMatriz(x)==True:
+     for lista in x:
+        if ordenados(lista)==True:
+            res.append(True)
+        else:
+            res.append(False)
+    
+            
+    return res
+
+
+
