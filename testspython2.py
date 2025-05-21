0597a6a9866c3946297156718b86eba3599f4297
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-05-21 19:48:06 -0300

    Update testspython2.py

--- testspython2.py
+++ testspython2.py
@@ -1,5 +1,5 @@
 import unittest
-from python2 import resultado_materia,saldoActual, pertenece_a_cadauno_1
+from python2 import resultado_materia,saldoActual, pertenece_a_cadauno_1,es_matriz,filas_ordenadas,columna,columnas_ordenadas,transponer
 class test_resultado_materia(unittest.TestCase):
     def test_todo_desaprobado(self):
         resultado_esperado =3 
@@ -29,6 +29,55 @@ class test_saldo_actual(unittest.TestCase):
     def test_saldo_actual_vacio(self):
         resultado_esperado=0
         self.assertEqual(saldoActual([]),resultado_esperado)
+
+
+class test_es_matriz(unittest.TestCase):
+    def test_es_matriz_listas_mismos_n_elementos(self):
+        self.assertTrue(es_matriz([[1,2],[6,7],[98,0]]))
+    def test_es_matriz_listas_distintos_n_elementos(self):
+        self.assertFalse(es_matriz([[1,2],[1]]))
+        
+    def test_matriz_vacia(self):
+        self.assertFalse(es_matriz([[]]))
+        
+class test_filas_ordenadas(unittest.TestCase):
+    def test_filas_ordenadas_correcto(self):
+        resultado_esperado= [True,False]
+        self.assertEqual(filas_ordenadas([[1,2],[2,0]]),resultado_esperado)
+        
+    def test_filas_ordenadas_vacio(self):
+        resultado_esperado = []
+        self.assertEqual(filas_ordenadas([[]]),resultado_esperado)
+
+
+class test_columna(unittest.TestCase):
+    def test_columna_matriz_vacia(self):
+        resultado_esperado = []
+        self.assertEqual(columna([[]],1),resultado_esperado)
+    def test_columna_esperado(self):
+        resultado_esperado= [1,3,1]
+        self.assertEqual(columna([[1,2],[3,4],[1,1]],0),resultado_esperado)
+    def test_columna_parametro_mal(self):
+        resultado_esperado = []
+        self.assertEqual(columna([[1,2],[1,1]],6),resultado_esperado)
+        
+class test_columnas_ordenadas(unittest.TestCase):
+    def test_columnas_correcto(self):
+        resultado_esperado = [True,False]
+        self.assertEqual(columnas_ordenadas([[1,2],[2,1]]),resultado_esperado)
+        
+    def test_columnas_vacio(self):
+        resultado_esperado=[]
+        self.assertEqual(columnas_ordenadas([[]]),resultado_esperado)
+        
+class test_transponer(unittest.TestCase):
+    def test_traspueta_correcta(self):
+        resultado_esperado=[[1,5,9],[2,6,10],[3,8,11]]
+        self.assertEqual(transponer([[1,2,3],[5,6,8],[9,10,11]]),resultado_esperado)
+        
+    def test_traspuesta_matriz_incorrecta(self):
+        resultado = []
+        self.assertEqual(transponer([[1,2],[3,5,6]]),resultado)
         
 
         
